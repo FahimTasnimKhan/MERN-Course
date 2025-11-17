40d42f4ef0b926c65164d73b0a92c4a777abc109
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2025-11-17 07:54:56 +0600

    Routes Added

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 const app = express();
 const port = 5001;
 
-app.get("/api/notes", (req, res) => {
-  res.status(200).send("20 notes");
-});
-app.post("/api/notes", (req, res) => {
-  res.status(201).json({ message: "post created successfully" });
-});
-app.put("/api/notes/:id", (req, res) => {
-  res.status(201).json({ message: "post updated successfully" });
-});
-app.delete("/api/notes", (req, res) => {
-  res.status(201).json({ message: "Note deleted successfully" });
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
