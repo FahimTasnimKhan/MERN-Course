9473f5c755f4b336b9196eeb6c10abe768b3abf9
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2025-11-17 07:42:13 +0600

    First Base Done

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,17 @@ import express from "express";
 const app = express();
 const port = 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/notes", (req, res) => {
+  res.status(200).send("20 notes");
+});
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "post created successfully" });
+});
+app.put("/api/notes/:id", (req, res) => {
+  res.status(201).json({ message: "post updated successfully" });
+});
+app.delete("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note deleted successfully" });
 });
 
 app.listen(port, () => {
