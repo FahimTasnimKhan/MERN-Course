ae37c4b3dad0f46928cc07c7a87b60abe022d851
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2025-11-18 07:09:35 +0600

    Danger-1

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://fahimtasnimkhan_db_user:cnMuzR7rpdXDU3nE@cluster0.16vdity.mongodb.net/?appName=Cluster0"
+      "mongodb+srv://fahimtasnimkhan_db_user:cnMuzR7rpdXDU3nE@cluster0.16vdity.mongodb.net/notes_db?appName=Cluster0"
     );
     console.log("MONGODB Connected Successfully ");
   } catch (error) {
     console.error("Error connecting to MONGOBD", error);
+    process.exit(1);
   }
 };
